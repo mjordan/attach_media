d3f080197d6ca70f8b8e304c29f0cacceb3466a8
Mark Jordan <mjordan@sfu.ca>
2020-11-11 18:19:17 +0000

    Third commit.

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 Islandora 8/9 module that provides a Drush command to attach a file to a node. Intended for use with files that are too large to upload using Drupal's standard web interface.
 
+This utility creates the File and Media entities in Drupal, but it does not touch the files themselves. In other words, if the filesystem schema is 'public://' or 'private://', the file must exist there already; if the schema is 'fedora://', the binary resource must exist in Fedora already. This is a feature, not a deficiency, since Drupal's FileSystem.php's saveData() uses PHP's `file_get_contents()` and `file_put_contents()`, which are limited to handling files that are smaller than the PHP's configured memory limits. This utility is intended to avoid using those PHP functions completely so files larger than PHP's limits can be used as Drupal media.
+
 ## Do not use
 
 This is a pre-alpha, proof of concept module. It might not even make it past this point. DON'T INSTALL IT YET.
--- src/Commands/IslandoraAttachMediaCommands.php
+++ src/Commands/IslandoraAttachMediaCommands.php
@@ -4,7 +4,9 @@ namespace Drupal\islandora_attach_media\Commands;
 
 use Drupal\node\Entity\Node;
 use Drupal\media\Entity\Media;
+use Drupal\file\Entity\File;
 use Drush\Commands\DrushCommands;
+// use Drupal\islandora\Flysystem\Fedora;
 
 /**
  * Drush commandfile.
@@ -12,13 +14,21 @@ use Drush\Commands\DrushCommands;
 class IslandoraAttachMediaCommands extends DrushCommands {
 
   /**
-   * Attaches the file to the node.
+   * Creates a media from a file and attaches the media to a node.
+   *
+   * This utility only creates the File and Media entities in Drupal, it does not
+   * touch the files themselves. In other words, if the filesystem schema is
+   * 'public://' or 'private://', the file must exist there already; if the schema
+   * is 'fedora://', the binary resource must exist in Fedora already. This is a
+   * feature, not a deficiency, since Drupal's FileSystem.php's saveData() uses
+   * file_put_contents(), which is limited to handling files that are smaller than
+   * the PHP's configured memory limits.
    *
    * @param string $nid
    *   The node ID.
    *
    * @param string $source_file_path
-   *   The absolute path to the source file.
+   *   The absolute path to the source file, or URL to the file in Fedora.
    *
    * @command islandora_attach_media:attach
    * @usage islandora_attach_media:attach 100 /tmp/image.jpg
@@ -28,25 +38,62 @@ class IslandoraAttachMediaCommands extends DrushCommands {
       $this->logger()->error(dt('Node @nid not found.', ['@nid' => $nid]));
       exit();
     }
+    /*
+    // @todo: Ping Fedora URL to confirm the file is there.
     if (!file_exists($source_file_path)) {
       $this->logger()->error(dt('File @source_file_path not found.', ['@source_file_path' => $source_file_path]));
       exit();
     }
+     */
 
-    // @todo: Figure out best way to read large files. But...to quote Bender, "We're boned."
-    // core/lib/Drupal/Core/File/FileSystem.php's saveData() uses file_put_contents(),
-    // which is, along with file_get_contents(), what we need to avoid because they read the
-    // entire file into memory. saveData() is called regardless of whether the destination
-    // file system is local or Fedora.
-    
-    $data = file_get_contents($source_file_path);
+    // D9: $real_path = \Drupal\Core\File\FileSystem::realpath($source_file_path);
+    $real_path = drupal_realpath($source_file_path);
+    // Thows error: $real_path = \Drupal\islandora\Flysystem\Fedora::getExternalUrl($source_file_path);
+
     $filename = basename($source_file_path);
-    $file = file_save_data($data, 'public://' . basename($filename), FILE_EXISTS_REPLACE);
-    // $file = file_save_data($data, 'fedora://' . basename($filename), FILE_EXISTS_REPLACE);
+
+    $uri = 'public://nocopy.txt';
+    // $uri = 'fedora://2020-09/article.pdf';
+
+    // Create a file entity.
+    $file = File::create([
+      'uri' => $uri,
+      'uid' => \Drupal::currentUser()->id(),
+      // 'filesize' => use filesize() here for local files, or get hasSize property from Fedora.
+      'status' => FILE_STATUS_PERMANENT,
+    ]);
+/*
+    // Copied from https://api.drupal.org/api/drupal/core!modules!file!file.module/function/file_save_data/8.8.x.
+    // If we are replacing an existing file re-use its database record.
+    // @todo Do not create a new entity in order to update it. See
+    //   https://www.drupal.org/node/2241865.
+    if ($replace == FILE_EXISTS_REPLACE) {
+      $existing_files = entity_load_multiple_by_properties('file', array(
+        'uri' => $uri,
+      ));
+      if (count($existing_files)) {
+        $existing = reset($existing_files);
+        $file->fid = $existing
+          ->id();
+        $file
+          ->setOriginalId($existing
+          ->id());
+        $file
+          ->setFilename($existing
+          ->getFilename());
+      }
+    }
+    elseif ($replace == FILE_EXISTS_RENAME && is_file($destination)) {
+      $file
+        ->setFilename(drupal_basename($destination));
+    }
+*/
+    $file->save();
 
     // @todo: 'bundle' is the Media type. We need to be able to support all of them, and even new ones.
     $media = Media::create([
       'bundle' => 'file',
+      // @todo: Provide a Drush option for the user ID.
       // 'uid' => \Drupal::currentUser()->id(),
       'uid' => 1,
       // @todo: The field varies by Media type.
